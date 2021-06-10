c54f47b9d4839ea365fb22e1fd9e1e5c68d2447b
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-06-10 13:22:48 +0530

    Routing Added to the UI Project.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './App.css';
+import {Home} from './Components/home';
+import {AddUser} from './Components/addUser';
+import {EditUser} from './Components/editUser';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
     return (
         <React.Fragment>
-            <h1> Hello World!</h1>
+            <Router>
+                {/*Put the navigation bar here.*/}
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/add" component={AddUser}/>
+                    <Route exact path="/edit/:id" component={EditUser}/>
+                </Switch>
+            </Router>
         </React.Fragment>
     )
 }
---
+++ src/Components/editUser.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export const EditUser = () => {
+    return (
+        <div>
+            <h1>Edit User Component</h1>
+        </div>
+    )
+}
